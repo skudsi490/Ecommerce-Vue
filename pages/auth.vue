09bcd58cf7bf935792dfec553d4ec86abd7f483d
Sami Kudsi <skudsi490@gmail.com>
2024-03-13 01:29:08 +0100

    Fixing img PATH

--- pages/auth.vue
+++ pages/auth.vue
@@ -2,7 +2,7 @@
     <div id="AuthPage" class="w-full h-[100vh] bg-white">
         <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
             <NuxtLink to="/" class="min-w-[170px]">
-                <img width="170" src="/AliExpress-logo.png">
+                <img width="170" src="/DealDeal.png">
             </NuxtLink>
         </div>
 
