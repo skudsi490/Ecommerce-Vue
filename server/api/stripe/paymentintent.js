d63d9737fb6db9e1848185c757fedceebd30b9ed
Sami Kudsi <skudsi490@gmail.com>
2024-03-13 03:18:39 +0100

    Debugging stripe v1

--- server/api/stripe/paymentintent.js
+++ server/api/stripe/paymentintent.js
@@ -4,7 +4,7 @@ export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     const config = useRuntimeConfig(); 
     console.log('Stripe Secret Key:', config.private.stripeSk); 
-    const stripe = new Stripe(config.private.stripeSk); 
+    const stripe = new Stripe(String(config.private.stripeSk)); 
 
     return await stripe.paymentIntents.create({
         amount: Number(body.amount),
